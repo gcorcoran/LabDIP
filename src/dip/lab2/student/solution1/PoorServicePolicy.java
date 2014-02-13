166a9549058fd1b0cb4af63c64ffddc767fa6c15
Greg Corcoran <gcorcoran@my.wctc.edu>
2014-02-12 18:48:23 -0600

    Started playing around with lab 2, doesn't seem to be anywhere near completion.

---
+++ src/dip/lab2/student/solution1/PoorServicePolicy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author Pip
+ */
+public class PoorServicePolicy implements ServiceQuality {
+
+    @Override
+    public double getRate() {
+        return 0.10;
+    }
+    
+    
+}
---
+++ src/dip/lab2/student/solution1/TipCalculator.java
@@ -0,0 +1,17 @@
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author Pip
+ */
+public interface TipCalculator {
+
+    ServiceQuality getServiceQuality();
+
+    double getTip();
+
+    void setBill(double billAmt);
+
+    
+    
+}
